63ad998d7d1f1e926ba679406059b46404048e31
Samantha Gaytant <samantha.gaytant@laposte.net>
2025-12-11 19:56:14 +0100

    Update myApp.js to serve index.html

--- myApp.js
+++ myApp.js
@@ -1,5 +1,16 @@
 let express = require('express');
 let app = express();
+let path = require('path');
+
+// Sert le dossier public pour le CSS
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Route principale
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+module.exports = app;
 
 
 
